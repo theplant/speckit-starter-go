d2d4ed45b533566d3918e91d0756058a4900f768
Felix Sun <sunfmin@gmail.com>
2025-11-26 21:08:09 +0800

    Add update-speckit.sh script

---
+++ update-speckit.sh
@@ -0,0 +1,4 @@
+uvx --from git+https://github.com/github/spec-kit.git specify init --force --script=sh --ai=cursor-agent .
+git checkout HEAD -- .specify/templates/
+git checkout HEAD -- .specify/memory
+
